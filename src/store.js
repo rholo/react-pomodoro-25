2d56b75d99ee4ccde91e6090e6468dd6626caf4d
rholo <hola@rholo.cl>
2022-01-29 00:28:40 -0300

    feat(pomodoro): create store

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import React, { createContext, useReducer } from 'react';
+
+const initialState = {
+  timer: 25,
+  intervals: 3,
+};
+
+const store = createContext(initialState);
+
+const { Provider } = store;
+
+const StateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'add':
+        return state;
+      default:
+        throw new Error();
+    }
+  }, initialState);
+
+  return <Provider value={{ state, dispatch }}>{children}</Provider>;
+};
+
+export { store, StateProvider };
