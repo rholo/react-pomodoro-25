c25bad3cd74cfa1095c20bf46ca633605b6af061
Rodrigo Ahumada <github@rholo.cl>
2022-02-06 13:28:29 -0300

    chore: create reducers

--- src/store.tsx
+++ src/store.tsx
@@ -1,13 +1,5 @@
-import React, { createContext, useReducer } from 'react';
-
-type IState = {
-  timer: Number,
-  minutes: Number,
-  intervals: Number,
-  session: Number,
-  started: Boolean,
-  breath: Boolean
-}
+import React, { createContext, useReducer, Dispatch } from 'react';
+import {timerReducer} from './reducers';
 
 const initialState = {
   timer: 3,
@@ -23,39 +15,7 @@ const store = createContext(initialState);
 const { Provider } = store;
 
 const StateProvider = ({ children }: any) => {
-  const [state, dispatch] = useReducer((state: IState, action: any) => {
-    switch (action.type) {
-      case 'RESTART':
-        return {...initialState}
-      case 'TOGGLE_START': {
-        return { ...state, started: !state.started };
-      }
-      case 'TOGGLE_BREATH': 
-        return {...state, breath: !state.breath};
-      case 'SET_MINUTES':
-        return {...state, minutes: !state.breath ? state.timer : state.intervals}
-      case 'COUNTDOWN':
-        return { ...state, minutes: state.minutes > 0 ? state.minutes - 1 : 0 };
-      case 'INCREMENT_SESSION':
-        return { ...state, session: state.session + 1 };
-      case 'DECREMENT_SESSION':
-        return { ...state, session: state.session > 0 ? state.session - 1 : 0 };
-      case 'INCREMENT_INTERVALS':
-        return { ...state, intervals: state.intervals + 1 };
-      case 'DECREMENT_INTERVALS':
-        return {
-          ...state,
-          intervals: state.intervals > 0 ? state.intervals - 1 : 0,
-        };
-      case 'INCREMENT_TIMER':
-        return { ...state, timer: state.timer + 1, minutes: state.timer + 1 };
-      case 'DECREMENT_TIMER':
-        const timer = state.timer > 0 ? state.timer - 1 : 0;
-        return { ...state, timer, minutes: timer };
-      default:
-        throw new Error();
-    }
-  }, initialState);
+  const [state, dispatch] = useReducer(timerReducer, initialState);
 
   return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
