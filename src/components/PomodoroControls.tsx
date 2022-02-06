3ce32cd67f1df6e8a0428a72aaae0f38637571d2
rholo <rahumada@adere.so>
2022-02-06 19:56:39 -0300

    refactor(*): update useContext types

--- src/components/PomodoroControls.tsx
+++ src/components/PomodoroControls.tsx
@@ -3,26 +3,26 @@ import Controls from './Controls';
 import { store } from '../store';
 
 const PomodoroControls = () => {
-  const { state, dispatch } = useContext<any>(store);
+  const { state, dispatch } = useContext(store);
   return (
     <div>
       <Controls
         name="Working"
         timer={state.timer}
-        add={() => dispatch({ type: 'INCREMENT_TIMER' })}
-        subtract={() => dispatch({ type: 'DECREMENT_TIMER' })}
+        add={() => dispatch({ type: 'INCREASE_TIMER' })}
+        subtract={() => dispatch({ type: 'DECREASE_TIMER' })}
       />
       <Controls
         name="Breaks"
         timer={state.intervals}
-        add={() => dispatch({ type: 'INCREMENT_INTERVALS' })}
-        subtract={() => dispatch({ type: 'DECREMENT_INTERVALS' })}
+        add={() => dispatch({ type: 'INCREASE_INTERVALS' })}
+        subtract={() => dispatch({ type: 'DECREASE_INTERVALS' })}
       />
       <Controls
         name="Session"
         timer={state.session}
-        add={() => dispatch({ type: 'INCREMENT_SESSION' })}
-        subtract={() => dispatch({ type: 'DECREMENT_SESSION' })}
+        add={() => dispatch({ type: 'DECREASE_SESSION' })}
+        subtract={() => dispatch({ type: 'INCREASE_SESSION' })}
       />
     </div>
   );
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -4,7 +4,7 @@ import useInterval from '../hooks/useInterval';
 import { Number, Button } from './styled';
 
 const Timer = () => {
-  const { state, dispatch } = useContext<any>(store);
+  const { state, dispatch } = useContext(store);
   const { session, started, minutes, breath } = state;
   const [seconds, setSeconds] = useState(5);
   const [buttonLabel, setButtonLabel] = useState('Start');
@@ -16,9 +16,8 @@ const Timer = () => {
         if (minutes === 0) {
           dispatch({type:'TOGGLE_BREATH'})
           dispatch({type: 'SET_MINUTES'})
-          console.log({minutes, seconds, session, breath})
           if (breath) {
-            dispatch({type: 'DECREMENT_SESSION'})
+            dispatch({type: 'DECREASE_SESSION'})
           }
           if (session === 0) {
             startPause()
